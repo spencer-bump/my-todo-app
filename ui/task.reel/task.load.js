56f7435c45d002b2b7275a1596145ff14a1c2657
spencer-bump <noreply>
2015-01-12 23:20:37 +0000

    Publish build

---
+++ ui/task.reel/task.load.js
@@ -0,0 +1,1 @@
+montageDefine("aaa166b","ui/task.reel/task",{dependencies:["montage/ui/component"],factory:function(e,t){var n=e("montage/ui/component").Component;t.Task=n.specialize({constructor:{value:function(){this.super()}}})}});
